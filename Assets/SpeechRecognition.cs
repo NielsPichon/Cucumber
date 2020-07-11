99d1d1c3f0d4db38f6242cfce48688343e356b9b
DevinaVR <nimipi@nanotech.dtu.dk>
2020-07-11 09:53:55 +0100

    initial commit

---
+++ Assets/SpeechRecognition.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+
+/// <summary>
+/// see here https://lightbuzz.com/speech-recognition-unity/
+/// </summary>
+public class SpeechRecognition : MonoBehaviour
+{
+    public List<string> availableKeywords;
+
+    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+    public float speed = 1;
+
+    public Text results;
+
+    protected PhraseRecognizer recognizer;
+    protected string word = "";
+
+    private string[] keywords = new string[6];
+
+    private void Start()
+    {
+        if (availableKeywords.Count < 5)
+            throw new System.Exception("need at least 5 keywords");
+
+        // Random word choice from the available keywords
+        List<int> buffer = new List<int>();
+        for (int i = 0; i < 5; i++)
+        {
+            int idx = Random.Range(0, availableKeywords.Count);
+
+            while (buffer.Contains(idx))
+                idx = Random.Range(0, availableKeywords.Count);
+
+            keywords[i] = availableKeywords[idx];
+            buffer.Add(idx);
+        }
+
+        keywords[5] = "no";
+
+
+        if (keywords != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+            Debug.Log(recognizer.IsRunning);
+        }
+
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+    }
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        word = args.text;
+        results.text = "You said: <b>" + word + "</b>";
+    }
+
+    private void Update()
+    {
+        playerMovement movement = (GetComponent<playerMovement>() as playerMovement);
+
+        if (word == keywords[0])
+            movement.GoLeft();
+        else if (word == keywords[1])
+            movement.GoRight();
+        else if (word == keywords[2])
+            movement.GoUp();
+        else if (word == keywords[3])
+            movement.GoDown();
+        else if (word == keywords[4])
+            movement.Stop();
+        else if (word == keywords[5])
+            movement.Revert();
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movementSpeed = 1;
+
+    private Vector3 direction = new Vector3(0, 0, 0);
+
+    private float revertMvt = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GoUp()
+    {
+        direction = new Vector3(0, 0, 1);
+    }
+
+    public void GoDown()
+    {
+        direction = new Vector3(0, -1, 0);
+    }
+
+    public void GoLeft()
+    {
+        direction = new Vector3(-1, 0, 0);
+    }
+
+    public void GoRight()
+    {
+        direction = new Vector3(1, 0, 0);
+    }
+
+    public void Stop()
+    {
+        direction = new Vector3(0, 0, 0);
+    }
+
+    public void Revert()
+    {
+        revertMvt *= -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * movementSpeed * Time.deltaTime * revertMvt;
+    }
+}
